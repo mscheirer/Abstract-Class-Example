a2f5e591b1c94e0f86f8d20b197c2f2d5ccfca1e
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-05-04 18:58:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+// C# program to show the  
+// working of abstract class 
+using System;
+
+// Driver Class 
+public class main_method
+{
+    // Main Method 
+    public static void Main()
+    {
+        // 'g' is object of class 
+        // 'ValidationRuleBase' class ' 
+        // ValidationRuleBase' cannot 
+        // be instantiate 
+        ValidationRuleBase g;
+
+        // instantiate class 'Validation1' 
+        g = new Validation1();
+
+        // call 'ValidationRule()' of class 'Validation1' 
+        g.ValidationRule();
+
+        // instantiate class 'Validation2'   
+        g = new Validation2();
+
+        // call 'ValidationRule()' of class 'Validation2' 
+        g.ValidationRule();
+
+    }
+}
+
---
+++ ValidationRuleBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+// abstract class 'ValidationRuleBase' 
+public abstract class ValidationRuleBase
+{
+    // abstract method 'ValidationRule()' 
+    public abstract void ValidationRule();
+}
+
+// class 'ValidationRuleBase' inherit 
+// in child class 'Validation1' 
+public class Validation1 : ValidationRuleBase
+{
+    // abstract method 'ValidationRule()'  
+    // declare here with  
+    // 'override' keyword 
+    public override void ValidationRule()
+    {
+        Console.WriteLine("class Validation1");
+    }
+}
+
+// class 'ValidationRuleBase' inherit in  
+// another child class 'Validation2' 
+public class Validation2 : ValidationRuleBase
+{
+    // same as the previous class 
+    public override void ValidationRule()
+    {
+        Console.WriteLine("class Validation2");
+    }
+}
+
+
